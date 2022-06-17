00d7abc2550fd2bac2b3f74192c701c69a3d5fc1
teerasej <teerasej@gmail.com>
2022-06-17 21:15:09 +0700

    A page that access provider and show user's data

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_3_random_user_with_provider/model/user_model.dart';
+import 'package:flutter_3_random_user_with_provider/provider/random_user_provider.dart';
+import 'package:provider/provider.dart';
 
 class DetailPage extends StatelessWidget {
   const DetailPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    UserModel selectedUser = context.read<RandomUserProvider>().selectedUser!;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(selectedUser.name),
+      ),
+      body: Column(
+        children: [
+          ListTile(
+            leading: const Icon(
+              Icons.phone,
+              color: Colors.green,
+            ),
+            title: Text(selectedUser.phone),
+            onTap: () {},
+          )
+        ],
+      ),
+    );
   }
 }
