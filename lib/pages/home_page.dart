a07a5defef45b6a809d418dd7004fe155930fd5a
teerasej <teerasej@gmail.com>
2022-06-17 21:15:39 +0700

    access provider

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_3_random_user_with_provider/provider/random_user_provider.dart';
 
 import '../model/user_model.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
@@ -9,6 +11,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var provider = context.read<RandomUserProvider>();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
