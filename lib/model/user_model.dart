9741c844200e34fe4fd789107218a32f2141b0ca
teerasej <teerasej@gmail.com>
2022-06-17 21:14:00 +0700

    represent user's info from api

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,1 +1,11 @@
-class UserModel {}
+class UserModel {
+  String name = "";
+  String phone = "";
+  String pictureUrl = "";
+
+  UserModel.parseMap(Map<String, dynamic> randomUser) {
+    name = randomUser['name']['first'];
+    pictureUrl = randomUser['picture']['large'];
+    phone = randomUser['phone'];
+  }
+}
